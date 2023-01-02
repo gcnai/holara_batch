4aecc4c38513fe577603347d08ca65e6a04dbfc1
gc <a@>
2023-01-02 21:37:05 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Holaraで一括ダウンロードできないのを何とかするスクリプト
+
+## 使い方
+
+1. F12 を押してデベロッパーコンソールを開いてNetworkタブを出しっぱなしにする。
+2. 画像を生成する。
+3. "Save all as HAR with content"でHAR形式で保存する。
+4. HARファイルをmain.pyにぶちこむ。
+
+## できること
+
+Networkタブを開いた状態で生成した画像をローカルに吐き出す。その際にpromptなど情報をPNGのメタデータに組み込む。
+
+## 動作環境
+
+- 動いた: vivaldi
+- 多分動く: Chrome
+- 動かない: Firefox
+  - jsonのテキストの最大長が10Mしかない
+
+## Usage
+
+```shell
+python main.py holara.ai.har
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+import base64
+import io
+import json
+import sys
+import urllib.parse
+from datetime import datetime
+import PIL.Image
+import PIL.PngImagePlugin
+
+
+def array_to_dict(a):
+    return dict(zip(map(lambda i: i['name'], a), map(lambda i: i['value'], a)))
+
+
+def array_to_dict2(a):
+    return dict(zip(map(lambda i: i['name'], a), map(lambda i: urllib.parse.unquote_plus(i['value']), a)))
+
+
+if __name__ == '__main__':
+    with io.open(sys.argv[1], "rb") as fp:
+        j = json.load(fp)
+
+        def entry_filter(e):
+            return e['request']['url'] == 'https://holara.ai/holara/api/1.0/generate_image'\
+                and e['response']['status'] == 200
+
+        for entry in filter(entry_filter, j["log"]["entries"]):
+            content = json.loads(entry['response']['content']['text'])
+            if content['status'] != 'success':
+                continue
+            headers = array_to_dict(entry['response']['headers'])
+            dt = datetime.strptime(headers['date'], '%a, %d %b %Y %H:%M:%S %Z')
+            params = array_to_dict2(entry['request']['postData']['params'])
+            metaStr = "%s\nNegative prompt: %s\nSteps: %s, Sampler: , CFG scale: %s, Seed: %s, Size: %sx%s" \
+                      % (params['prompt'], params['negative_prompt'], params['steps'], params['cfg_scale'], params['seed'], params['width'], params['height'])
+            i = 0
+            for im in content['images']:
+                name = "%s_%02d.png" % (dt.strftime("%Y%d%H%M%S"), i)
+                data = base64.b64decode(im)
+                with PIL.Image.open(io.BytesIO(data)) as pim:
+                    info = PIL.PngImagePlugin.PngInfo()
+                    info.add_text('parameters', metaStr)
+                    with io.open(name, "wb") as f:
+                        pim.save(f, pnginfo=info)
